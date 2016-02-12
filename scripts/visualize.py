a57063cd97a73cb4d35c1a25471f18935cd24a50
Hannes Ovrén <hannes.ovren@liu.se>
2016-02-12 14:10:09 +0100

    Move useful scripts to subdirectory

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -6,6 +6,8 @@ from rsimusim.dataset import Dataset
 
 parser = argparse.ArgumentParser()
 parser.add_argument("dataset")
+parser.add_argument("--axes", action='store_true')
+parser.add_argument("--orientations", action='store_true')
 args = parser.parse_args()
 
 print('Loading from', args.dataset)
@@ -13,4 +15,4 @@ t0 = time.time()
 ds = Dataset.from_file(args.dataset)
 elapsed = time.time() - t0
 print('Loaded {:d} landmarks in {:.1f} seconds'.format(len(ds.landmarks), elapsed))
-ds.visualize()
+ds.visualize(draw_axes=args.axes, draw_orientations=args.orientations)
